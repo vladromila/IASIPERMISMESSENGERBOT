b0220f9227f59db3e13f0ffa735759e84e832de4
Vlad Romila <vladromila@MacBook-Pro.local>
2022-01-20 19:51:54 +0200

    Done

--- app.js
+++ app.js
@@ -32,8 +32,8 @@ bot.on('message', (payload, chat) => {
   // Send a list template
   chat.say({
     elements: [
-      { title: 'Artile 1', image_url: '/path/to/image1.png', default_action: {} },
-      { title: 'Artile 2', image_url: '/path/to/image2.png', default_action: {} }
+      { title: 'Artile 1', image_url: 'https://cdn.volkswagen.ro/media/Content_SimpleMedia_Image_Component/9701-113486-74968-113487-child/dh-995-e2a2c0/f5a616c0/1633005169/premiera.jpg', default_action: {} },
+      { title: 'Artile 2', image_url: 'https://cdn.volkswagen.ro/media/Content_SimpleMedia_Image_Component/9701-113486-74968-113487-child/dh-995-e2a2c0/f5a616c0/1633005169/premiera.jpg', default_action: {} }
     ],
     buttons: [
       { type: 'postback', title: 'View More', payload: 'VIEW_MORE' }
