d9b0d642674b4dd1565a33368f6f7dd329238e2e
Vlad Romila <vladromila@MacBook-Pro.local>
2022-01-20 20:02:13 +0200

    Done

--- app.js
+++ app.js
@@ -9,45 +9,21 @@ const bot = new BootBot({
   port: 5555
 });
 
-bot.setPersistentMenu([
-  {
-    title: 'My Account',
-    type: 'nested',
-    call_to_actions: [
-      {
-        title: 'Pay Bill',
-        type: 'postback',
-        payload: 'PAYBILL_PAYLOAD'
-      },
-      {
-        title: 'History',
-        type: 'postback',
-        payload: 'HISTORY_PAYLOAD'
-      },
-      {
-        title: 'Contact Info',
-        type: 'postback',
-        payload: 'CONTACT_INFO_PAYLOAD'
-      }
+bot.setGetStartedButton(() => {
+  chat.say({
+    text: 'Bună ziua! Cu ce vă putem ajuta?',
+    buttons: [
+      { type: 'postback', title: 'Prețuri', payload: 'HELP_PREȚURI' },
+      { type: 'postback', title: 'Mașini', payload: 'HELP_MAȘINI' },
+      { type: 'postback', title: 'Instructori', payload: 'HELP_INSTRUCTORI' }
     ]
-  },
-  {
-    title: 'Go to Website',
-    type: 'web_url',
-    url: 'http://purple.com'
-  }
-], true);
+  });
+})
+
 
 bot.on('postback:PAYBILL_PAYLOAD', (payload, chat) => {
   chat.say(`Pay Bill here...`);
 });
 
-bot.on('postback:HISTORY_PAYLOAD', (payload, chat) => {
-  chat.say(`History here...`);
-});
-
-bot.on('postback:CONTACT_INFO_PAYLOAD', (payload, chat) => {
-  chat.say(`Contact info here...`);
-});
 
 bot.start(process.env.PORT);
