e52388319b102ad3dc84acd0318a156af2cdbdd1
Vlad Romila <vladromila@MacBook-Pro.local>
2022-01-20 19:48:21 +0200

    Done

--- app.js
+++ app.js
@@ -11,7 +11,12 @@ const bot = new BootBot({
 
 bot.on('message', (payload, chat) => {
   const text = payload.message.text;
-  chat.say(`Echo: ${text}`);
+  chat.say({
+    cards: [
+      { title: 'Volkswagen Golf 6', image_url: 'https://www.chicagotribune.com/resizer/coI0JHa_AAGTW3lcMXxSYjsli10=/1200x0/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/53E7MBRVDZHVNBUZPDLOWAXQ3I.jpg', subtitle: "Cutie de viteze: manuală" },
+      { title: 'Card 2', image_url: 'https://www.chicagotribune.com/resizer/coI0JHa_AAGTW3lcMXxSYjsli10=/1200x0/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/53E7MBRVDZHVNBUZPDLOWAXQ3I.jpg', default_action: {} }
+    ]
+  });
 });
 
 bot.start(process.env.PORT);
