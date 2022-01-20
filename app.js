49a12a80ebb47efd8843716935b6a4d7f56510fd
Vlad Romila <vladromila@MacBook-Pro.local>
2022-01-20 21:31:24 +0200

    Done

--- app.js
+++ app.js
@@ -188,6 +188,71 @@ function handlePostback(senderPsid, receivedPostback) {
       }
       break;
     case "BUTTON_PRICES":
+      response = {
+        "attachment": {
+          "type": "template",
+          "payload": {
+            "template_type": "list",
+            "top_element_style": "compact",
+            "elements": [
+              {
+                "title": "Classic T-Shirt Collection",
+                "subtitle": "See all our colors",
+                "image_url": "https://originalcoastclothing.com/img/collection.png",
+                "buttons": [
+                  {
+                    "title": "View",
+                    "type": "web_url",
+                    "url": "https://originalcoastclothing.com/collection",
+                    "messenger_extensions": true,
+                    "webview_height_ratio": "tall",
+                    "fallback_url": "https://originalcoastclothing.com/"
+                  }
+                ]
+              },
+              {
+                "title": "Classic White T-Shirt",
+                "subtitle": "See all our colors",
+                "default_action": {
+                  "type": "web_url",
+                  "url": "https://originalcoastclothing.com/view?item=100",
+                  "messenger_extensions": false,
+                  "webview_height_ratio": "tall"
+                }
+              },
+              {
+                "title": "Classic Blue T-Shirt",
+                "image_url": "https://originalcoastclothing.com/img/blue-t-shirt.png",
+                "subtitle": "100% Cotton, 200% Comfortable",
+                "default_action": {
+                  "type": "web_url",
+                  "url": "https://originalcoastclothing.com/view?item=101",
+                  "messenger_extensions": true,
+                  "webview_height_ratio": "tall",
+                  "fallback_url": "https://originalcoastclothing.com/"
+                },
+                "buttons": [
+                  {
+                    "title": "Shop Now",
+                    "type": "web_url",
+                    "url": "https://originalcoastclothing.com/shop?item=101",
+                    "messenger_extensions": true,
+                    "webview_height_ratio": "tall",
+                    "fallback_url": "https://originalcoastclothing.com/"
+                  }
+                ]
+              }
+            ],
+            "buttons": [
+              {
+                "title": "View More",
+                "type": "postback",
+                "payload": "payload"
+              }
+            ]
+          }
+        }
+      }
       break;
     case "BUTTON_CARS_AVAILABLE":
       break;
